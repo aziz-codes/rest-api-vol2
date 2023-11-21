8ffd381f7bceb0aad68a983e0e8bf105e58679c3
aziz-codes <azizisoffline@gmail.com>
2023-11-21 21:58:15 +0500

    created an instance to listen for user route

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from "express";
-
+import useRoutes from "./routes/users.js";
 import bodyParser from "body-parser";
 
 const PORT = 5000;
 
 const app = express();
 // middleware to handle http requests..
-app.use(bodyParser);
+app.use(bodyParser.json());
+
+app.use("/users", useRoutes);
+
+app.get("/", (_, res) => res.send("helo"));
 
 app.listen(PORT, () => console.log("server is running on server : " + PORT));
