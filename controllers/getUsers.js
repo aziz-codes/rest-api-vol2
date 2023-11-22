0cb3226166b4ceb674e18941436e30c3698da347
aziz-codes <azizisoffline@gmail.com>
2023-11-22 23:59:50 +0500

    get user contorler

--- controllers/getUsers.js
+++ controllers/getUsers.js
@@ -1,25 +1,5 @@
-export const getUsers = async (req, res) => {
-  try {
-    // Make a GET request to JSONPlaceholder using fetch
-    const response = await fetch("https://jsonplaceholder.typicode.com/users");
-
-    // Check if the request was successful (status code 200)
-    if (response.ok) {
-      // Parse the JSON data from the response
-      const users = await response.json();
-
-      // Send the users data to the client
-      res.json(users);
-    } else {
-      // If the response status is not OK, handle the error
-      console.error("Error fetching data:", response.statusText);
-      res.status(response.status).send("Internal Server Error");
-    }
-  } catch (error) {
-    // Handle other errors
-    console.error("Error fetching data:", error.message);
-    res.status(500).send("Internal Server Error");
-  }
+export const getUsers = (req, res) => {
+  //  user schema
 };
 
 export const createUser = (req, res) => {
