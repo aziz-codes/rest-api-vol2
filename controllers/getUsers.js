0a50fae10be6badef06926c780a171f1296b096e
aziz-codes <azizisoffline@gmail.com>
2023-11-27 22:40:12 +0500

    changed controller and routes meta

--- controllers/getUsers.js
+++
@@ -1,8 +0,0 @@
-export const getUsers = (req, res) => {
-  //  user schema
-  res.send("all users route");
-};
-
-export const createUser = (req, res) => {
-  res.send("create user route");
-};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getUsers, createUser } from "../controllers/getUsers.js";
+import { getUsers, createUser } from "../controllers/user.js";
 
 const router = express.Router();
 // this will be route for /users
 router.get("/", getUsers);
-router.post("/create-user", createUser);
+router.post("/signup", createUser);
 
 export default router;
