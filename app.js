bc4833ddd7698bf50ba8bb8b5d0942ac7e50643a
aziz-codes <azizisoffline@gmail.com>
2023-11-27 22:39:25 +0500

    connected to mongoDB

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 import express from "express";
 import useRoutes from "./routes/users.js";
 import bodyParser from "body-parser";
-
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 const PORT = 5000;
 
 const app = express();
 // middleware to handle http requests..
 app.use(bodyParser.json());
+dotenv.config();
+const URL = process.env.MONGODB_URL;
+mongoose.connect(URL);
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDB");
+});
 
 app.use("/users", useRoutes);
 
